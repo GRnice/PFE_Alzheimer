60933756ffd4f9efefae9068accddba732ea75ef
chebaane-PC\chebaane <meriemchebaane93@gmail.com>
2016-12-01 16:21:52 +0200

    Cacher les icones lors de chaque clique sur le profile: version2.

--- ApplicationTablette/newproj/www/js/controllers.js
+++ ApplicationTablette/newproj/www/js/controllers.js
@@ -108,7 +108,7 @@ angular.module('starter.controllers', ['ionic'])
       for (var i=0;i<$scope.profilsSelected.length;i++){
           if($scope.profilsSelected[i].id==id){
               $scope.profilsSelected[i].highlighted= !$scope.profilsSelected[i].highlighted;
-              $scope.profilsSelected[i].idImage=false;
+              $scope.profilsSelected[i].idImage=!$scope.profilsSelected[i].idImage;
           }
           else{
               $scope.profilsSelected[i].highlighted=false;
