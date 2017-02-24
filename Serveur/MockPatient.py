819c4678ccd44eac4345a3998633d136892a523e
Dominique <dominique.dib@gmail.com>
2017-02-24 13:54:59 +0100

    new circles

--- Serveur/MockPatient.py
+++ Serveur/MockPatient.py
@@ -10,23 +10,23 @@ sock.recv(4096) # attente du OKPROMENADE
 input("envoyer premiere position")
 sock.send("POSITION*7.079131*43.612139*100\r\n".encode())  ## 43.612139, 7.079131
 input("envoyer deuxieme position")
-sock.send("POSITION*7.079684*43.612120*100\r\n".encode())  ## 43.612120, 7.079684
+sock.send("POSITION*7.079275*43.612615*100\r\n".encode())  ## 43.612615, 7.079275
 messageOuiOuNon = input("Activer alerte sortie de zone ? y/n")
 if messageOuiOuNon == "y":
-    sock.send("POSITION*7.079676*43.612066*100\r\n".encode())    ##43.612489, 7.079131
+    sock.send("POSITION*7.079240*43.612685*100\r\n".encode())    ## 43.612685, 7.079240
     time.sleep(0.5)
 
 input("envoyer Avant derniere position")
-sock.send("POSITION*7.079791*43.612153*100\r\n".encode())  ## 43.612153, 7.079791
+sock.send("POSITION*7.079324*43.612672*100\r\n".encode())  ## 43.612672, 7.079324
 
 
 messageOuiOuNon = input("Activer alerte sortie de zone ? y/n")
 if messageOuiOuNon == "y":
-    sock.send("POSITION*7.079754*43.612062*100\r\n".encode())    ##43.611987, 7.079584
+    sock.send("POSITION*7.079157*43.612685*100\r\n".encode())    ## 43.612256, 7.080074
     time.sleep(0.5)
 
 input("envoyer derniere position")
-sock.send("POSITION*7.079708*43.612136*100\r\n".encode())  ## 43.612136, 7.079708
+sock.send("POSITION*7.079235*43.612590*100\r\n".encode())  ## 43.612590, 7.079235
 
 
 input("envoyer stop suivi et fermer le socket")
@@ -46,11 +46,23 @@ sock.close()
 ##43.612111, 7.079108 - 43.612076, 7.079142 -
 
 ## hors zone droite, red:
-# 43.612066, 7.079676 - 43.612062, 7.079754
+# 43.612103, 7.079829 - 43.612062, 7.079754
 
 
+## hors zone south droite gray:
+#43.611931, 7.079408 - 43.611931, 7.079366
 
+## hors zone gauche bas, black:
+## 43.612256, 7.078941 - 43.612281, 7.078919 - 43.612244, 7.078957
 
+## hors zone haut gauche Cyan:
+#43.612761, 7.079305 - 43.612758, 7.079347 - 43.612741, 7.079342
 
-#Reste
-#7.079869,43.611771,7.079730,43.612091
+## hors zone haut droite Bleu:
+##43.612264, 7.080148 - 43.612251, 7.080125
+
+## hors zone north, un peu a gauche: - WHITE
+## 43.612690, 7.079596 - 43.612714, 7.079563
+
+## hors zone gauche en haut, apres le magenta, RED;
+#43.612685, 7.079240 -  43.612646, 7.079157
